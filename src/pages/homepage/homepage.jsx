30b2efa0fe44ad6b1c322fe5363604e228459f67
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-30 13:41:49 +0545

    updating folder structure and adding directory/ menu-item components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory';
+
+import './homepage.scss';
+
+export default function homepage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
