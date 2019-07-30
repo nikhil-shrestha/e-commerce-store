d0ef1c0a5f00de6eeafe3f001c04848b07449f48
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-30 15:52:34 +0545

    creating sign-in components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in';
+
+import './sign-in-and-sign-up.scss';
+
+const signInAndAignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default signInAndAignUpPage;
