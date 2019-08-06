9e6c22f1d17c567971ee1d4039f781e8e7c3aa1d
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-08-06 09:12:25 +0545

    implementing redux in cart

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { toogleCartHidden } from '../../redux/cart/cart.actions';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.scss';
 
-export default function CartIcon() {
+function cartIcon({ toogleCartHidden }) {
   return (
-    <div className="cart-icon">
+    <div className="cart-icon" onClick={toogleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
   );
 }
+
+const mapDispatchToProps = dispatch => ({
+  toogleCartHidden: () => dispatch(toogleCartHidden())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(cartIcon);
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,5 @@
+import { CartActionType } from './cart.types';
+
+export const toogleCartHidden = () => ({
+  type: CartActionType.TOGGLE_CART_HIDDEN
+});
