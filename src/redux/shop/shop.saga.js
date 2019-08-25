e7a9e1f18912a50eccd28c5e0a1486cf6862d266
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-08-25 09:26:02 +0545

    creating generator function, and added root saga

---
+++ src/redux/shop/shop.saga.js
@@ -0,0 +1,36 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import ShopActionTypes from './shop.types';
+import {
+  fetchCollectionsSucess,
+  fetchCollectionsFailure
+} from './shop.actions';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
+
+export function* fetchCollectionsAsync() {
+  yield console.log('I am fired');
+
+  try {
+    const collectionRef = firestore.collection('collections');
+
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSucess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
